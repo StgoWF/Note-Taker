c2f3b09fb460814609817327596a3babdd39d848
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-04-30 10:27:42 -0400

    Add routes to serve HTML pages for main and notes

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
 });
+
+
+const path = require('path');
+
+// Route to serve the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Route to serve the notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
