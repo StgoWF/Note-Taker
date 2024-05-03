29bc4d26a20cf803bd1703f3d3a98f57bbc154f6
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-05-03 11:29:33 -0400

    problem with deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory located in 'Develop'
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'Develop', 'public')));
 
 // Route to serve the main page
 app.get('/', (req, res) => {
